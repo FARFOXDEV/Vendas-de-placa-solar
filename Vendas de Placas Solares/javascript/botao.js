bf77b8d4188097373c7cd749cdda6eb285defb9f
FARFOXDEV <151898025+FARFOXDEV@users.noreply.github.com>
2024-03-06 14:54:27 -0300

    Add files via upload

---
+++ Vendas de Placas Solares/javascript/botao.js
@@ -0,0 +1,9 @@
+document.getElementById('toggleMenu').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+  });
+  
+  document.getElementById('closeMenu').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+  });
+
+  
---
+++ Vendas de Placas Solares/javascript/usuario.js
@@ -0,0 +1,8 @@
+function openOverlay() {
+    document.getElementById("overlay").style.display = "flex";
+  }
+  
+  function closeOverlay() {
+    document.getElementById("overlay").style.display = "none";
+  }
+  
